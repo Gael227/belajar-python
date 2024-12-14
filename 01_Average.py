258007b63b23024e504420b60106316dfc0f54a8
Gael227 <nazaki2207@gmail.com>
2024-12-14 14:21:03 +0700

    mulai belajar python

---
+++ 01_Average.py
@@ -0,0 +1,7 @@
+print('program ini menghitung rata2 dari 2 angka')
+
+num1 = float(input("masukkan angka pertama "))
+num2 = float (input("masukkan angka kedua "))
+
+print('angkanya adalah',num1,'dan',num2)
+print('rata2nya adalah: ',(num1 + num2) / 2)
---
+++ 02_Km_to_Miles_Converter.py
@@ -0,0 +1,3 @@
+km = float(input("input KM: "))
+miles = km / 1.609344
+print(km, "km sama dengan: ", round(miles,4), "miles")
