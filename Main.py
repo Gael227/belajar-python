4cb4092371797b17305ae6af3239604ce77eba80
Gael227 <nazaki2207@gmail.com>
2024-12-17 21:06:31 +0700

    wip todolist

--- Main.py
+++ Main.py
@@ -1,4 +1,7 @@
+#array kosong yg akan diisi di function addList
 toDoList = []
+
+#function untuk memasukkan input user ke dalam array toDoList
 def addList():
     while True:
         isi = input("Add to list: ")
@@ -12,7 +15,9 @@ print('2. delete item in list')
 print('3. show list')
 print('4. keluar')
 
-inp = int(input(print('choose one option: ')))
+#variabel yg menyimpan hasil input user
+inp = int(input('choose one option: '))
 
+#percabangan berdasarkan input user
 if inp == 1:
     addList()
---
+++ tes.py
@@ -0,0 +1,19 @@
+#if elif pada string
+print('---if/elif pada string')
+a = 'gedebog'
+if 'gedebog' in a:
+    print('gedebog pisang')
+elif 'bakwan' in a:
+    print('bakwan jagung')
+else:
+    print('apcb')
+
+#menampilkan karakter dari index [x1 s/d x2]
+print('---slicing')
+b = 'phild corn'
+print(b[2:8])
+print(b[:5])
+print(b[6:])
+
+
+
